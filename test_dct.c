f2338a4290979f1d1790230baa82a024cce8f776
Jake <jakefriesen@uvic.ca>
2022-06-06 15:18:12 -0700

    fixed sqrt2 macro

--- test_dct.c
+++ test_dct.c
@@ -6,7 +6,7 @@
 #define SIN1 0.19509032
 #define SIN3 0.55557023
 #define SIN6 0.92387953
-#define SQRT2 1.1421356
+#define SQRT2 1.41421356
 #define SQRT8 2.8284271
 #define PI 3.1415927
 
@@ -31,13 +31,13 @@ int leoffler (float *line_arr){
     stage2[3] = stage1[0] - stage1[3];
     stage2[4] = stage1[4]*COS3 + stage1[7]*SIN3;
     stage2[5] = stage1[5]*COS1 + stage1[6]*SIN1;
-    stage2[6] = -stage1[5]*SIN1 + stage1[6]*COS1;
-    stage2[7] = -stage1[4]*SIN3 + stage1[7]*COS3;
+    stage2[6] = (-1.0)*stage1[5]*SIN1 + stage1[6]*COS1;
+    stage2[7] = (-1.0)*stage1[4]*SIN3 + stage1[7]*COS3;
     //Stage 3
     stage3[0] = stage2[0] + stage2[1];
     stage3[1] = stage2[0] - stage2[1];
     stage3[2] = SQRT2*COS6*stage2[2] + SQRT2*SIN6*stage2[3];
-    stage3[3] = -SQRT2*SIN6*stage2[2] + SQRT2*COS6*stage2[3];
+    stage3[3] = (-1.0)*SQRT2*SIN6*stage2[2] + SQRT2*COS6*stage2[3];
     stage3[4] = stage2[4] + stage2[6];
     stage3[6] = stage2[4] - stage2[6];
     stage3[5] = stage2[7] - stage2[5];
