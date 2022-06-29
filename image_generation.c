613702f8dd596db9349828dfbf947e0a19a6a456
Jake <jakefriesen@uvic.ca>
2022-06-29 13:27:37 -0700

    Added Header file for image gen, cleared up binaries

--- image_generation.c
+++ image_generation.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "image_generation.h"
 
 /****************************************************
 Image Generation
@@ -221,24 +222,24 @@ int print_matrix(u_int8_t* matrix)
 }
 
 //TODO: This main should be removed once testing is finished
-int main(void)
-{
-    printf("Image Generation Tests");
-    u_int16_t width = 16;
-    u_int16_t height = 8;
-    u_int8_t test_image[width][height];
-    u_int8_t new_image[width][height];
-    u_int8_t matrix [8][8];
-    image_gen(width, height, (u_int8_t*)test_image);
-    print_image(width, height, (u_int8_t*)test_image);
-    save_to_file(width, height, (u_int8_t*)test_image, "Image");
-    load_from_file("Image", (u_int8_t*)new_image);
-    print_image(width, height, (u_int8_t*)new_image);
-    save_to_file(width, height, (u_int8_t*)new_image, "New_Image");
-    get_matrix((u_int8_t*)new_image, width, height, 0, 0, (u_int8_t*)matrix);
-    print_matrix((u_int8_t*)matrix);
-    matrix[0][0] = 111;
-    put_matrix((u_int8_t*)new_image, width, height, 0, 0, (u_int8_t*)matrix);
-    print_image(width, height, (u_int8_t*)new_image);
-    return 0;
-}
+// int main(void)
+// {
+//     printf("Image Generation Tests");
+//     u_int16_t width = 16;
+//     u_int16_t height = 8;
+//     u_int8_t test_image[width][height];
+//     u_int8_t new_image[width][height];
+//     u_int8_t matrix [8][8];
+//     image_gen(width, height, (u_int8_t*)test_image);
+//     print_image(width, height, (u_int8_t*)test_image);
+//     save_to_file(width, height, (u_int8_t*)test_image, "Image");
+//     load_from_file("Image", (u_int8_t*)new_image);
+//     print_image(width, height, (u_int8_t*)new_image);
+//     save_to_file(width, height, (u_int8_t*)new_image, "New_Image");
+//     get_matrix((u_int8_t*)new_image, width, height, 0, 0, (u_int8_t*)matrix);
+//     print_matrix((u_int8_t*)matrix);
+//     matrix[0][0] = 111;
+//     put_matrix((u_int8_t*)new_image, width, height, 0, 0, (u_int8_t*)matrix);
+//     print_image(width, height, (u_int8_t*)new_image);
+//     return 0;
+// }
