9f2cde18b4ee205e36b70f26785eec9d3f5940f4
Jake <jakefriesen@uvic.ca>
2022-07-09 16:29:09 -0700

    2d fixed point dct with 16bit results and sf of 5

--- dct_optimized.c
+++ dct_optimized.c
@@ -21,10 +21,9 @@ int fbutterfly(float in[2], float out[2], float constant[2]){
 
 
 int loeffler_opt (int16_t *line_arr){
-    //TODO: Just have one array of intermediate values (not array?)
-    //TODO: Split up even and odd parts to reduce temporary variables
-    int32_t temp1, temp2;
-    int16_t local1, local2, local3, local4;
+    //TODO: pass in image pointer and positions of 1d array
+    int32_t temp1, temp2; //32bit temp variables to accomodate larger values before rounding
+    int16_t local1, local2, local3, local4; //16bit local variables to manipulate and copy back to line_arr
 
     //Stage 1 - Even Section
     local1 = line_arr[0] + line_arr[7];  //9-bit
@@ -44,80 +43,109 @@ int loeffler_opt (int16_t *line_arr){
     temp1 = local1 + local2;                //11-bit
     local2 = local1 - local2;               //11-bit
     local1 = temp1;
+
     temp1 = SQRT2COS6*local3;               //10*8bit SF 2^8
     temp1 = ((temp1 >> 1) | (temp1 & 1));   //SF 2^7 with vn rounding
     temp2 = SQRT2SIN6*local4;               //10*8bit SF 2^7
-    temp2 = (temp1 + temp2) >> 2;           //SF 2^5
-    temp1 = -SQRT2SIN6*local3;              //10*8bit AF 2^7 - this is moved ahead of local3 = temp2;
+    temp2 = (temp1 + temp2) >> 6;           //SF 2^1
+    temp1 = -SQRT2SIN6*local3;              //10*8bit SF 2^7 - this is moved ahead of local3 = temp2;
     local3 = temp2;
     temp2 = SQRT2COS6*local4;               //10*8bit SF 2^8
     temp2 = ((temp2 >> 1) | (temp2 & 1));   //SF 2^7 with vn rounding
-    local4 = (temp1 + temp2) >> 2;          //SF 2^5
+    local4 = (temp1 + temp2) >> 6;          //SF 2^1
 
     //Stage 4 - Even Section AND Stage 1 - Odd Section
     temp1 = local1;
     local1 = line_arr[3] - line_arr[4];  //9-bit
-    line_arr[4] = local2;                //11bit, SF 0
+    line_arr[4] = local2 << 1;           //12bit, SF 1
     local2 = line_arr[2] - line_arr[5];  //9-bit
-    line_arr[2] = local3;                //16bit, SF 5
+    line_arr[2] = local3;                //12bit, SF 1
     local3 = line_arr[1] - line_arr[6];  //9-bit
-    line_arr[6] = local4;                //16bit, SF 5
+    line_arr[6] = local4;                //12bit, SF 1
     local4 = line_arr[0] - line_arr[7];  //9-bit
-    line_arr[0] = temp1;                 //11bit, SF 0
+    line_arr[0] = temp1 << 1;            //12bit, SF 1
 
     //Stage 2 - Odd Section
     temp1 = local1*COS3FP;            //9*8bit 
     temp2 = local4*SIN3FP;            //9*8bit 
-    temp2 = (temp1 + temp2) >> 2;     //15bit SF 6
+    temp2 = (temp1 + temp2) >> 5;     //12bit SF 1
     temp1 = local1*SIN3FP;            //9*8bit 
     local1 = temp2;
     temp2 = local4*COS3FP;            //9*8bit 
-    local4 = (temp2 - temp1) >> 2;    //15bit SF 6
+    local4 = (temp2 - temp1) >> 5;    //12bit SF 1
 
     temp1 = local2*COS1FP;            //9*8bit
     temp2 = local3*SIN1FP;            //9*8bit
-    temp2 = (temp1 + temp2) >> 2;    //15bit SF 6
+    temp2 = (temp1 + temp2) >> 5;    //12bit SF 1
     temp1 = local2*SIN1FP;            //9*8bit
     local2 = temp2;
     temp2 = local3*COS1FP;            //9*8bit
-    local3 = (temp2 - temp1) >> 2;    //15bit SF 6
+    local3 = (temp2 - temp1) >> 5;    //12bit SF 1
 
     //Stage 3 - Odd Section
-    temp1 = local1 + local3;      //16-bit SF 6
-    local3 = local1 - local3;     //16-bit SF 6
+    temp1 = (local1 + local3) >> 1;      //12-bit SF 1
+    local3 = (local1 - local3) >> 1;     //12-bit SF 1
     local1 = temp1;
-    temp1 = local4 - local2;      //16-bit SF 6
-    local4 = local4 + local2;     //16-bit SF 6
+    temp1 = (local4 - local2) >> 1;      //12-bit SF 1
+    local4 = (local4 + local2) >> 1;     //12-bit SF 1
     local2 = temp1;
 
     //Stage 4 - Odd Section
     temp1 = (local4 - local1) >> 1;
     temp2 = (local4 + local1) >> 1;
-    line_arr[7] = temp1; //16bit, SF 5
-    line_arr[1] = temp2; //16bit, SF 5
-    line_arr[3] = (local2*SQRT2FP) >> 9;     //16bit, SF 5
-    line_arr[5] = (local3*SQRT2FP) >> 9;     //16bit, SF 5
+    line_arr[7] = temp1; //12bit, SF 1
+    line_arr[1] = temp2; //12bit, SF 1
+    line_arr[3] = (local2*SQRT2FP) >> 9;     //12bit, SF 1
+    line_arr[5] = (local3*SQRT2FP) >> 9;     //12bit, SF 1
 
     return 1;
 }
 
 int main(void){
     int16_t test_arr [8] = {255, 0, 100, 50, 255, 30, 255, 0};
+    int16_t test_arr3[8][8] = {{10, 20, 30, 40, 50, 60, 70, 80}, {10, 20, 30, 40, 50, 60, 70, 80}, {255, 0, 100, 50, 255, 30, 255, 0}, {10, 20, 30, 40, 50, 60, 70, 80},
+                            {255, 0, 100, 50, 255, 30, 255, 0}, {10, 20, 30, 40, 50, 60, 70, 80}, {10, 20, 30, 40, 50, 60, 70, 80}, {10, 20, 30, 40, 50, 60, 70, 80}};
+    int16_t buf [8];
     //Expected output without scaling   
     //945.0000   52.2862    2.3214  434.1773  175.0000  332.3058 -190.3802  637.3610
-    int sf [8] = {0,5,5,5,0,5,5,5};
+    int sf [8] = {1,1,1,1,1,1,1,1};
+    int sf2d [8] = {2,2,2,2,2,2,2,2};
+    //1D
+    for(int i = 0; i < 8; i++){
+        loeffler_opt(test_arr3[i]);
+    }
+    for(int i = 0; i < 8; i++){
+        for(int j = 0; j < 8; j++){
+            buf[j] = test_arr3[j][i];
+        }
+        loeffler_opt(buf);
+        for(int j = 0; j < 8; j++){
+            test_arr3[j][i] = buf[j];
+        }
+    }
+    printf("2D Fixed point results\n");
+    for(int i = 0; i < 8; i++){
+        for(int j = 0; j < 8; j++){
+            float f = (float)test_arr3[i][j] / (1<<5);
+            printf("%.2f ", f );
+        }
+        printf("\n");
+    }
 
-    loeffler_opt(test_arr);
-    printf("Results with SF included\n");
-    for(int i = 0; i < 8; i++){
-        printf("%d ", test_arr[i]);
-    }
-    printf("\n Results with SF removed, floats\n");
-    for(int i = 0; i < 8; i++){
-        float f = (float)test_arr[i] / (1<<(sf[i]));
-        printf("%.3f ", f);
-    }
-    printf("\n");
+
+
+
+    // loeffler_opt(test_arr);
+    // printf("Results with SF included\n");
+    // for(int i = 0; i < 8; i++){
+    //     printf("%d ", test_arr[i]);
+    // }
+    // printf("\n Results with SF removed, floats\n");
+    // for(int i = 0; i < 8; i++){
+    //     float f = (float)test_arr[i] / (1<<(sf[i]));
+    //     printf("%.3f ", f);
+    // }
+    // printf("\n");
 }
 /*
 1-D DCT:
@@ -135,5 +163,17 @@ i=4 : 175.000000
 i=5 : 332.305847 
 i=6 : -190.380173 
 i=7 : 637.361023 
+
+*/
+/*
+  506.2500 -123.5908    0.5803   94.2582   43.7500   78.8147  -47.5950  158.2647
+   37.2788   14.9435    0.1479   28.8815   11.1518   21.5381  -12.1319   40.7069
+ -135.1174  -54.1630   -0.5362 -104.6814  -40.4197  -78.0651   43.9721 -147.5424
+  -43.9733  -17.6271   -0.1745  -34.0681  -13.1544  -25.4059   14.3105  -48.0170
+         0         0         0         0         0         0         0         0
+  -65.8108  -26.3808   -0.2611  -50.9865  -19.6870  -38.0227   21.4172  -71.8625
+   55.9675   22.4351    0.2221   43.3605   16.7424   32.3356  -18.2138   61.1141
+  187.4132   75.1263    0.7437  145.1973   56.0638  108.2794  -60.9910  204.6472
+
 
 */
--- makefile
+++ makefile
@@ -4,7 +4,7 @@ native : CFLAGS= -static
 arm : CC=arm-linux-gnueabi-gcc
 arm : CFLAGS= -static
 
-binaries= test_dct test_dct_arm test_image_gen dct_image_compression
+binaries= test_dct test_dct_arm test_image_gen dct_image_compression test_optimize
 assembly = test_dct_arm dct_image_compression_arm
 images = image.pgm Decompressed_Image.pgm Compressed_Image.pgm
 
@@ -19,6 +19,8 @@ arm : dct_image_compression.c image_generation.c
 image_gen : image_generation.c test_image_gen.c
 	$(CC) -o test_image_gen $(CFLAGS) test_image_gen.c image_generation.c
 
+opt_dct : dct_optimized.c image_generation.c
+	$(CC) -o test_optimize $(CFLAGS) dct_optimized.c image_generation.c
 .PHONY: clean
 
 clean: 
